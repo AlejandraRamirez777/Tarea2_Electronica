cc5a634375426671e7702e60695f3b4f227cf341
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-09-20 00:07:45 -0500

    Vo

--- Tarea2_Vo.py
+++ Tarea2_Vo.py
@@ -28,21 +28,66 @@ zero = np.zeros(100)
 A=0
 for i in IIm1:
     if i<0.01 and i>-0.01:
-        print "Vc1 = " + str(VVin[A])
+        print "Vm1 = " + str(VVin[A])
     A+=1
 
 B=0
 for i in IIm2:
     if i<0.01 and i>-0.01:
-        print "Vc2 = " + str(VVin[B])
+        print "Vm2 = " + str(VVin[B])
     B+=1
+
+Id1 = IIm2 - IIm1
+
+C=0
+for i in Id1:
+    if i<0.01 and i>-0.01:
+        print "Vd1 = " + str(VVin[B])
+    C+=1
 
 #Graficas
 plt.plot(VVin,IIm1, label = "Im1")
 plt.plot(VVin,IIm2, c = "r", label = "Im2")
+plt.plot(VVin,Id1, c = "g", label = "Id1")
 plt.plot(VVin, zero, c = "k")
 plt.legend(loc = 2)
 plt.title("Im1 y Im2 vs Vin")
-plt.xlabel("Voltaje in (Vin) [V]")
+plt.xlabel("Voltaje en (Vin) [V]")
 plt.ylabel("Corriente [mA]")
 plt.savefig("Im1_Im2__Vin.png")
+plt.clf()
+
+#Constantes
+Vc1 = -1000.0
+Vc2 = 17.9
+R1 = 10000.0
+R2 = R1
+R3 = R1
+V1 = 10.0
+V2 = 5.0
+Vg = 0.7
+
+#Funcion de Vo
+def Vo(Vin):
+    ans = np.array([])
+    for i in Vin:
+        if i < Vc2:
+            ans = np.append(ans,i)
+        else:
+            a = ((R3/(R3+R1))*i) - Vg + V2
+            ans = np.append(ans,a)
+    return ans
+
+#Linea de Vc2
+one = np.ones(100)
+vcc2 = one*Vc2
+
+#Grafica
+plt.plot(VVin,Vo(VVin))
+plt.plot(vcc2,VVin, c = "y", label = "Vc2")
+plt.legend(loc = 2)
+plt.title("Vo vs Vin")
+plt.xlabel("Voltaje en Vin [V]")
+plt.ylabel("Voltaje en Vo [V]")
+plt.savefig("Vo_vs_Vin.png")
+plt.clf()
