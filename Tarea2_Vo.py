34c56356b5312d7ec9f3b6bd8a42061065b705f0
AlejandraRamirez <ma.ramirez@uniandes.edu.co>
2018-09-20 19:24:44 -0500

    Ok

--- Tarea2_Vo.py
+++ Tarea2_Vo.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 #Im1 en funcion de Vin
 def Im1(Vin):
-    a = (1/15.0)*(Vin+9.3)
-    b = (1/30.0)*(-13.6)
+    a = (1/15.0)*(Vin+10.7)
+    b = (1/30.0)*(-6.4)
     ans = a+b
     return ans
 
 #Im2 en funcion de Vin
 def Im2(Vin):
-    a = (1/30.0)*(Vin+9.3)
-    b = (1/15.0)*(-13.6)
+    a = (1/30.0)*(Vin+10.7)
+    b = (1/15.0)*(-6.4)
     ans = a+b
     return ans
 
@@ -25,6 +25,10 @@ IIm2 = Im2(VVin)
 #Linea de I = 0
 zero = np.zeros(100)
 
+Id1 = IIm2 - IIm1
+Id2 = IIm2
+
+"""
 A=0
 for i in IIm1:
     if i<0.01 and i>-0.01:
@@ -44,10 +48,11 @@ for i in Id1:
     if i<0.01 and i>-0.01:
         print "Vd1 = " + str(VVin[B])
     C+=1
+"""
 
 #Graficas
-plt.plot(VVin,IIm1, label = "Im1")
-plt.plot(VVin,IIm2, c = "r", label = "Im2")
+#plt.plot(VVin,IIm1, label = "Im1")
+plt.plot(VVin,IIm2, c = "r", label = "Id2")
 plt.plot(VVin,Id1, c = "g", label = "Id1")
 plt.plot(VVin, zero, c = "k")
 plt.legend(loc = 2)
@@ -58,8 +63,8 @@ plt.savefig("Im1_Im2__Vin.png")
 plt.clf()
 
 #Constantes
-Vc1 = -1000.0
-Vc2 = 17.9
+Vc1 = -10.7
+Vc2 = 5.7
 R1 = 10000.0
 R2 = R1
 R3 = R1
@@ -71,19 +76,25 @@ Vg = 0.7
 def Vo(Vin):
     ans = np.array([])
     for i in Vin:
-        if i < Vc2:
+        if  i < Vc1:
+            a = ((R2/(R2+R1))*i) - Vg - V2
+            ans = np.append(ans,a)
+        elif  Vc1 < i < Vc2:
             ans = np.append(ans,i)
         else:
-            a = ((R3/(R3+R1))*i) - Vg + V2
-            ans = np.append(ans,a)
+            b = ((R3/(R3+R1))*i) + Vg + V2
+            ans = np.append(ans,b)
     return ans
 
-#Linea de Vc2
+#Linea de Vc1
 one = np.ones(100)
+vcc1 = one*Vc1
+#Linea de Vc2
 vcc2 = one*Vc2
 
 #Grafica
-plt.plot(VVin,Vo(VVin))
+plt.plot(VVin,Vo(VVin),c ="k")
+plt.plot(vcc1,VVin, c = "c", label = "Vc1")
 plt.plot(vcc2,VVin, c = "y", label = "Vc2")
 plt.legend(loc = 2)
 plt.title("Vo vs Vin")
@@ -92,14 +103,24 @@ plt.ylabel("Voltaje en Vo [V]")
 plt.savefig("Vo_vs_Vin.png")
 plt.clf()
 
+
+#Regiones D1 y D2
+
 Vv1 = np.ones(100)*V1
 Vv2 = np.ones(100)*V2
 
+#Voltaje Diodo1
 Vd1 = -Vv1  - VVin
-Vd2 = VVin + Vv2
+#Voltaje Diodo2
+Vd2 = VVin - Vv2
 
-plt.plot(VVin,Vd2, label = "Vd2")
+#Linea de Vg = 0.7
+Vvg = np.ones(100)*Vg
+
+#Graficas
 plt.plot(VVin,Vd1, c = "y", label = "Vd1")
+plt.plot(VVin,Vd2, c = "r", label = "Vd2")
+plt.plot(VVin, Vvg, c = "g")
 plt.plot(VVin, zero, c = "k")
 plt.legend(loc = 2)
 plt.title("Voltaje diodos vs Vin")
